b4801c88f3744cf49030f7ea6d0049695b465789
RyanYensch <ry2005@outlook.com>
2025-01-22 03:16:28 +1100

    initialised the window

--- simulator.cpp
+++ simulator.cpp
@@ -1,7 +1,18 @@
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
 
-int main() {
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+#define WIN_TITLE "3D Engine"
 
+int main() {
+    // Initialising the window
+    if (!glfwInit()) return -1;
+    GLFWwindow* window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL);
+    if (!window) return -1;
+    glfwMakeContextCurrent(window);
+
+
+    glfwTerminate();
     return 0;
 }
