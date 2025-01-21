dc06a02f0783ff0d6b67dc44767847354c50d645
RyanYensch <ry2005@outlook.com>
2025-01-22 03:59:25 +1100

    update on rendering

--- engine.cpp
+++ engine.cpp
@@ -1,4 +1,5 @@
 #include "engine.h"
+#include <glad/glad.h>
 
 void PhysicsEngine::addObject(const RigidBody& obj) {
     objects.push_back(obj);
@@ -9,4 +10,22 @@ void PhysicsEngine::update(float deltaTime) {
         obj.velocity += obj.acceleration * deltaTime;
         obj.position += obj.velocity * deltaTime;
     }
+}
+
+void PhysicsEngine::render(const Camera& camera, int windowWidth, int windowHeight) {
+    // Calculation Matricies
+    glm::mat4 projection = glm::perspective(glm::radians(camera.fov), (float)windowWidth / windowHeight, 0.1f, 100f);
+    glm::mat4 view = camera.getViewMatrix();
+
+    //clear screen
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // render the objects
+    for (const auto& obj : objects) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, obj.position);
+
+        glm::mat4 mvp = projection * view * model;
+    }
+
 }
--- engine.h
+++ engine.h
@@ -4,6 +4,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <vector>
+#include <GLFW/glfw3.h>
 
 struct RigidBody {
     glm::vec3 position;
@@ -33,6 +34,7 @@ class PhysicsEngine {
 public:
     void addObject(const RigidBody& obj);
     void update(float deltaTime);
+    void render(const Camera& camera, int windowWidth, int windowHeight);
 
 private:
     std::vector<RigidBody> objects;
