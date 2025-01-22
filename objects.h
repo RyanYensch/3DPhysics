bc28c9d9c62e5710f6bc84dbed5ab8594832071c
RyanYensch <ry2005@outlook.com>
2025-01-22 20:16:30 +1100

    added enum class for shape, and initialise stub

--- objects.h
+++ objects.h
@@ -6,17 +6,43 @@
 #include <vector>
 #include <GLFW/glfw3.h>
 
+enum class ShapeType { 
+    Cuboid,
+    Sphere
+};
+
 class RigidBody {
 public:
     glm::vec3 position;
     glm::vec3 velocity;
     glm::vec3 acceleration;
+    glm::vec3 scale;
+    ShapeType shape;
     float mass;
-    float scale;
 
 
-    RigidBody(glm::vec3 pos, glm::vec3 vel, glm::vec3 acc, float m, float size)
-        : position(pos), velocity(vel), acceleration(acc), mass(m), scale(size) {}
+    RigidBody(glm::vec3 pos, glm::vec3 vel, glm::vec3 acc, glm::vec3 size, ShapeType shp, float m)
+        : position(pos), velocity(vel), acceleration(acc), mass(m), scale(size), shape(shp) {
+            initShape();
+        }
+
+
+private:
+    std::vector<glm::vec3> verticies;
+
+    void initShape() {
+        switch(shape) {
+            case ShapeType::Cuboid:
+                initCuboid();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void initCuboid() {
+        
+    }
 };
 
 
